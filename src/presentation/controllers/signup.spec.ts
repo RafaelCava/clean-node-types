d6429a6961d2820a705b994003c4f55429ddcdf3
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-18 13:53:39 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../erros/missing-param-error'
-import { InvalidParamError } from '../erros/invalid-param-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../erros'
 import { EmailValidator } from '../protocols/email-validator'
-import { ServerError } from '../erros/server-error'
 
 interface SutTypes {
   sut: SignUpController
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
-import { MissingParamError } from '../erros/missing-param-error'
+import { MissingParamError, InvalidParamError } from '../erros'
 import { HttpRequest, HttpResponse } from '../protocols/https'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../erros/invalid-param-error'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
