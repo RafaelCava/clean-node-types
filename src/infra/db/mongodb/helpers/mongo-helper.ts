84ca1e492952f68083f43ebf3377390c5c4e6042
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-19 17:58:10 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    } as any)
+  },
+  async disconnect () {
+    await this.client.close()
+  }
+}
