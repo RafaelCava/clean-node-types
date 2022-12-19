32ad79b7a41b9e355b15ebc4c49a8cc9a5d556a2
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-19 18:39:47 -0300

    feat: ensure AccountMongoRepository return an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -10,5 +10,8 @@ export const MongoHelper = {
   },
   async disconnect () {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
