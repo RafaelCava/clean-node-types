27d962eddbc268a2fd5224a5c04b9e425c9a690e
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-21 17:36:33 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -14,12 +14,9 @@ export class SignUpController implements Controller {
       if (error) {
         return badRequest(error)
       }
-      const { name, email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password } = httpRequest.body
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
-      }
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
       const account = await this.addAccount.add({
         name,
---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,11 @@
+import { InvalidParamError } from '../../erros'
+import { Validation } from './validation'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (private readonly fieldName: string, private readonly fieldToCompareName: string) {}
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName)
+    }
+  }
+}
