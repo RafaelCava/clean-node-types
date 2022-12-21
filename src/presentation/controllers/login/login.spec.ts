53af2fefa2eb723f0851aca5a8265ea4f97e5ecb
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-21 13:50:13 -0300

    test: ensure LoginController retuns 400 if an invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../erros'
+import { InvalidParamError, MissingParamError } from '../../erros'
 import { badRequest } from '../../helpers/http-helper'
 import { HttpRequest } from '../../protocols/https'
 import { EmailValidator } from '../signup/signup-protocols'
@@ -38,7 +38,8 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
-  test('Should returns 400 if no password is provided', async () => {
+
+  test('Should returns 400 if an invalid email is provided', async () => {
     const { sut } = makeSut()
     const httpRequest: HttpRequest = {
       body: {
@@ -47,6 +48,19 @@ describe('Login Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should returns 400 if no password is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 
   test('Should call EmailValidator with correct email', async () => {
