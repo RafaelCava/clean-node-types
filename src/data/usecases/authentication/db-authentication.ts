4596f25fd87df20971746bc5711b10b494b89237
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-22 12:31:18 -0300

    feat: insert new depencies on DbAuthentication - ensure calls TokenGenerator with correct id - ensure throw if TokenGenerator throws

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,11 +1,13 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
+import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -13,6 +15,7 @@ export class DbAuthentication implements Authentication {
     if (!account) return null
     const compare = await this.hashComparer.compare(authentication.password, account.password)
     if (!compare) return null
-    return 'fasd'
+    const accessToken = await this.tokenGenerator.generate(account.id)
+    return accessToken
   }
 }
