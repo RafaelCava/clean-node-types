fb05da2ef31a417b6f098fe9da76a596dc2321d8
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-22 11:50:55 -0300

    refactor: move Encrypter to criptography folder

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
-export * from '../../protocols/encrypter'
+export * from '../../protocols/criptography/encrypter'
 export * from '../../protocols/add-account-repository'
