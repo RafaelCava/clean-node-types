7b1fb04d0855677667f6eaf331cf86d7901639d6
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-23 11:53:25 -0300

    test: ensure JwtAdapter returns encrypt value on sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,23 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a encrypt value on sign succeeds', async () => {
+    const sut = new JwtAdapter('secret')
+    const encrypteValue = await sut.encrypt('any_id')
+    expect(encrypteValue).toBe('any_token')
   })
 })
