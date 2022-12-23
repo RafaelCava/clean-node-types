08f8f69d0f83efcbbd15e6745563e224e39b1b73
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-23 12:04:25 -0300

    refactor: rename update with updateAccessToken

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -21,7 +21,7 @@ export class DbAuthentication implements Authentication {
     const compare = await this.hashComparer.compare(authentication.password, account.password)
     if (!compare) return null
     const accessToken = await this.encrypter.encrypt(account.id)
-    await this.updateAccessTokenRepository.update(account.id, accessToken)
+    await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
     return accessToken
   }
 }
