6114284aba9fe52f0dc8eef82dac83898e7e8f00
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2022-12-24 19:43:12 -0300

    feat: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,14 +1,14 @@
+import { AuthMiddleware } from './auth-middleware'
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccountModel } from '../../domain/models/account'
-import { AuthMiddleware } from './auth-middleware'
 import { AccessDeniedError } from '../erros'
-import { forbidden } from '../helpers/http/http-helper'
 import { HttpRequest } from '../protocols'
+import { forbidden, serverError } from '../helpers/http/http-helper'
 
 const makeLoadAccountByTokenRepositoryStub = (): LoadAccountByToken => {
   class LoadAccountByToken implements LoadAccountByToken {
     async load (accessToken: string, role?: string): Promise<AccountModel> {
-      return new Promise(resolve => resolve(null as any))
+      return new Promise(resolve => resolve(makeFakeAccount()))
     }
   }
   return new LoadAccountByToken()
@@ -34,10 +34,25 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
 describe('Auth Middleware', () => {
-  test('Should return 403 if no x-access-token exists in headers', async () => {
+  test('Should return 403 if no headers are provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should return 403 if no x-access-token exists in headers', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      headers: {}
+    })
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 
@@ -46,5 +61,13 @@ describe('Auth Middleware', () => {
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('Should returns 500 if LoadAccountByToken throws', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    console.log(httpResponse)
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,17 +1,21 @@
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../erros'
-import { forbidden } from '../helpers/http/http-helper'
+import { forbidden, serverError } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols'
 import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.headers) return forbidden(new AccessDeniedError())
-    if (httpRequest.headers && !httpRequest.headers['x-access-token']) {
-      return forbidden(new AccessDeniedError())
+    try {
+      if (!httpRequest.headers) return forbidden(new AccessDeniedError())
+      if (httpRequest.headers && !httpRequest.headers['x-access-token']) {
+        return forbidden(new AccessDeniedError())
+      }
+      await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
+      return new Promise(resolve => resolve(null))
+    } catch (err) {
+      return serverError(err)
     }
-    await this.loadAccountByToken.load(httpRequest.headers['x-access-token'])
-    return new Promise(resolve => resolve(null))
   }
 }
