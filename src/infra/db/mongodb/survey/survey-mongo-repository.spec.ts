6e480731bc14158b86f68a39f019cdd374f0b64f
Rafael Cavalcante <82680356+RafaelCava@users.noreply.github.com>
2022-12-26 23:58:08 -0300

    develop (#15) - feature list surveys
    * chore: update docker compose file
    
    * chore: updare README
    
    * feat: ensure AddSurveyController call Validation with correct date
    
    * docs: add surveys requirements
    
    * feat: ensure LoadSurveysController calls LoadSurveys and returns 500 if LoadSurveys throws
    
    * feat: ensure LoadSurveysController returns 200 if LoadSurveys returns an surveys
    
    * feat: ensure LoadSurveyControler returns 200 on success
    
    * docs: update requirements
    
    * feat: ensure LoadSurveysController returns 204 if LoadSurveys returns empty
    
    * feat: ensure DbLoadSurveys calls LoadSurveysRepository
    
    * feat: ensure DbLoadSurveys throws if LoadSurveysRepository throws
    
    * feat: add mockdate
    
    * chore: update Readme
    
    * ci: insert new workflow
    
    * ci: update workflow install-and-test
    
    * chore: update Readme (#12)
    
    * feat: ensure SurveyMongoRepository returns a list of surveys on success
    
    * feat: ensure survey route GET returns 403 withou accessToken, 200 on load surveys an 204 without surveys
    
    * docs: update Requirements
    
    * chore: update Readme

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,6 +1,7 @@
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
+import MockDate from 'mockdate'
 
 let surveyCollection: Collection
 
@@ -11,9 +12,11 @@ const makeSut = (): SurveyMongoRepository => {
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as any)
+    MockDate.set(new Date())
   })
 
   afterAll(async () => {
+    MockDate.reset()
     await MongoHelper.disconnect()
   })
 
@@ -22,21 +25,54 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  test('Should add a survey on success', async () => {
-    const sut = makeSut()
-    const surveyData = {
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const sut = makeSut()
+      const surveyData = {
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }
+      await sut.add(surveyData)
+      const survey = await surveyCollection.findOne({ question: surveyData.question })
+      expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should return a list of surveys on success', async () => {
+      const sut = makeSut()
+      await surveyCollection.insertMany([{
+        question: 'any_question_1',
+        answers: [{
+          image: 'any_image_1',
+          answer: 'any_answer_1'
+        },
+        {
+          answer: 'other_answer_1'
+        }],
+        date: new Date()
       },
       {
-        answer: 'other_answer'
-      }],
-      date: new Date()
-    }
-    await sut.add(surveyData)
-    const survey = await surveyCollection.findOne({ question: surveyData.question })
-    expect(survey).toBeTruthy()
+        question: 'any_question_2',
+        answers: [{
+          image: 'any_image_2',
+          answer: 'any_answer_2'
+        },
+        {
+          answer: 'other_answer_2'
+        }],
+        date: new Date()
+      }])
+      const surveysFound = await surveyCollection.find().toArray()
+      const surveys = await sut.loadAll()
+      expect(surveys).toEqual(MongoHelper.map(surveysFound))
+    })
   })
 })
