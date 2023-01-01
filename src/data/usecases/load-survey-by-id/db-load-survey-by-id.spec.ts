ec8369752cb2c837729f4e73565ba0610a5d119b
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-01-01 14:42:13 -0300

    feat: ensure DbLoadSurveyById call LoadSurveyById with correct value

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,67 @@
+import { SurveyModel } from '@/domain/models/survey'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+import MockDate from 'mockdate'
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+
+const makeLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById (): Promise<SurveyModel> {
+      return new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadSurveyById
+  loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepository()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+  return {
+    sut,
+    loadSurveyByIdRepositoryStub
+  }
+}
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
+describe('DbLoadSurveyById UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyByIdRepository', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+    await sut.loadById('any_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test.skip('Should throws if LoadSurveysRepository throws', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockRejectedValueOnce(new Error())
+    const promise = sut.loadById('any_id')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test.skip('Should return a list with surveys on success', async () => {
+    const { sut } = makeSut()
+    const survey = await sut.loadById('any_id')
+    expect(survey).toEqual(makeFakeSurvey())
+  })
+})
