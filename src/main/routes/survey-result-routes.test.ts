2d87217d69d854170e589cad978ea6982bff8310
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-01-03 17:00:17 -0300

    test: ensure Survey Result Route PUT /surveys/:surveyId/results return 403 on save survey result with valid accessToken and invalid surveyId

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,13 +1,25 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
-// import { sign } from 'jsonwebtoken'
-// import env from '../config/env'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 import MockDate from 'mockdate'
 
 let surveyCollection: Collection
 let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const result = await accountCollection.insertOne({
+    name: 'Rafael',
+    email: 'rafael.cavalcante@gmail.com',
+    password: '1234',
+    role: 'admin'
+  })
+  const accessToken = sign({ id: result.insertedId }, env.jwtSecret)
+  await accountCollection.updateOne({ _id: result.insertedId }, { $set: { accessToken } })
+  return accessToken
+}
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -30,6 +42,17 @@ describe('Survey Result Routes', () => {
     test('Should return 403 on save survey result without accessToken', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+
+    test('Should return 403 on save survey result with valid accessToken and invalid surveyId', async () => {
+      const accessToken = await makeAccessToken()
+      await request(app)
+        .put(`/api/surveys/${new ObjectId().toString()}/results`)
+        .set('x-access-token', accessToken)
         .send({
           answer: 'any_answer'
         })
