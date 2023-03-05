9c69b116a0d19dfe47f75c72faf8823834b7a401
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2023-03-05 18:18:56 -0300

    feat: release/2.5.3

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
-    version: '2.5.2',
+    version: '2.5.3',
     license: {
       name: 'MIT',
       url: 'https://github.com/RafaelCava/clean-node-types/blob/master/LICENSE'
