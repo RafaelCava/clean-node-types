94e1811ced4f66a379c65be34eedeae33bf1e5d4
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2023-03-05 16:25:48 -0300

    docs: add load survey result path

--- src/main/docs/paths/singup-result-path.ts
+++ src/main/docs/paths/singup-result-path.ts
@@ -1,5 +1,8 @@
 export const signUpResultPath = {
   put: {
+    security: [{
+      apiKeyAuth: []
+    }],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
     parameters: [{
@@ -35,6 +38,39 @@ export const signUpResultPath = {
             $ref: '#/schemas/surveyResultAnswerParams'
           }
         }
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
       }
     }
   }
