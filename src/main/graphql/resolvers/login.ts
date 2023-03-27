2594093209629983a11efe6ddb4a0e1c82800215
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-03-27 12:03:25 -0300

    feat: make login resolver use login Controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,8 @@
+import { adaptResolver } from '@/main/adapters/apollo-server-resolver-adapter'
+import { makeLoginController } from '@/main/factories/controllers/login/login/login-controller-factory'
+
 export default {
   Query: {
-    login () {
-      return {
-        accessToken: 'any_token',
-        name: 'any_name'
-      }
-    }
+    login: async (parent: any, args: any) => await adaptResolver(makeLoginController(), args)
   }
 }
