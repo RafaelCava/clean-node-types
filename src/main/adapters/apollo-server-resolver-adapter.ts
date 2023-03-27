3dd1bc8d2030ae34b558b6df2af430abde653da9
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-03-27 13:00:34 -0300

    feat: add signUp mutation

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,6 +1,14 @@
 import { Controller } from '@/presentation/protocols'
+import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-express'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const response = await controller.handle(args)
-  return response.body
+  const strategies = {
+    200: () => response.body,
+    204: () => response.body,
+    400: () => { throw new UserInputError(response.body.message) },
+    401: () => { throw new AuthenticationError(response.body.message) },
+    403: () => { throw new ForbiddenError(response.body.message) }
+  }
+  return strategies[response.statusCode]() || (() => { throw new ApolloError(response.body.message) })()
 }
