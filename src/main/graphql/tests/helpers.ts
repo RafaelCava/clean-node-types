fef3bf8bc1c28aa5c17e577e66460a1e0a876ec2
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-01 15:53:48 -0300

    test: ensure login Query returns an account on valid credentials

---
+++ src/main/graphql/tests/helpers.ts
@@ -0,0 +1,44 @@
+import typeDefs from '@/main/graphql/type-defs'
+import resolvers from '@/main/graphql/resolvers'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { ApolloServer } from 'apollo-server-express'
+import { authDirectiveTransformer } from '../directives'
+import { GraphQLError } from 'graphql'
+
+let schema = makeExecutableSchema({ resolvers, typeDefs })
+schema = authDirectiveTransformer(schema)
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
+
+export const makeApolloServer = async (): Promise<ApolloServer> => {
+  const apolloServerSpy = new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req }),
+    plugins: [{
+      requestDidStart: async () => ({
+        willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
+  })
+
+  await apolloServerSpy.start()
+
+  return apolloServerSpy
+}
