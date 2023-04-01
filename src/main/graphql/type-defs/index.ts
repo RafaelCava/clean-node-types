759293913456ee6f621481c3c4e2367c946399e0
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-01 15:26:23 -0300

    feat: add SurveyResult query and mutation

--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,5 +1,6 @@
 import login from './login'
 import survey from './survey'
 import base from './base'
+import surveyResult from './survey-result'
 
-export default [base, login, survey]
+export default [base, login, survey, surveyResult]
