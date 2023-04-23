c74d4a649ce2af1a0282db36f462ddf129d24c91
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-23 12:48:48 -0300

    feat: release/3.0.1

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express'
-import { bodyParser, cors, contentType } from '../middlewares'
+import cors from 'cors'
+import { bodyParser, contentType } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
-  app.use(cors)
+  app.use(cors())
   app.use(contentType)
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,5 +1,4 @@
 /* istanbul ignore file */
 export * from './body-parser'
-export * from './cors'
 export * from './content-type'
 export * from './no-cache'
